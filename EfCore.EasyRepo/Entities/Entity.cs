182416afc15f877de1728b2ec9a52bde3ff13e1e
Chris Lee <chrislee187@gmail.com>
2024-09-17 20:19:41 +0100

    Switch to use Ulid for Guid Id's

--- EfCore.EasyRepo/Entities/Entity.cs
+++ EfCore.EasyRepo/Entities/Entity.cs
@@ -5,7 +5,7 @@ namespace EasyEF.Entities;
 [PrimaryKey("Id")]
 public abstract class Entity : IEntity
 {
-    public Guid Id { get; set; } = Guid.NewGuid();
+    public Guid Id { get; set; } = Ulid.NewUlid().ToGuid();
 }
 
 public interface IEntity
